b282725e897274dc4387197755b411557073a28f
Edison Pérez <edisonaqp@gmail.com>
2022-08-29 18:54:40 -0500

    eliminación de método duplicado

--- quadtree/quadtree.js
+++ quadtree/quadtree.js
@@ -28,26 +28,17 @@ class Rectangle {
   }
 }
 
-  // verifica si este objeto se intersecta con otro objeto Rectangle
-  intersects(range) {
-    if (
-      point.x == this.x - w ||
-      point.x == this.x + w ||
-      point.y == this.y - h ||
-      point.y == this.y + h
-    ) 
-      return true;
-        }
-            // verifica si este objeto contiene un objeto Punto
-    contains(point) {
-        if (point.x > (this.x - this.w) && point.x < (this.x + this.w) && point.y > (this.y - this.h) && point.y < (this.y + this.h)) {
-            return true;
-        }
-        return false;
+// verifica si este objeto se intersecta con otro objeto Rectangle
+intersects(range) {
+  if (
+    point.x == this.x - w ||
+    point.x == this.x + w ||
+    point.y == this.y - h ||
+    point.y == this.y + h
+  )
+    return true;
+}
 
- 
-    }
-  
 class QuadTree {
   constructor(boundary, n) {
     this.boundary = boundary; // Rectangle
@@ -56,58 +47,58 @@ class QuadTree {
     this.divided = false;
   }
 }
-  // divide el quadtree en 4 quadtrees
-  subdivide() {
-    // Algoritmo
-    // 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast ,  qt_southwest;
+// divide el quadtree en 4 quadtrees
+subdivide() {
+  // Algoritmo
+  // 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast ,  qt_southwest;
 
-    // 2: Asignar los QuadTree creados a cada hijo
-    this.northeast = qt_northeast;
-    this.northwest = qt_northwest;
-    this.southeast = qt_southeast;
-    this.southwest = qt_southwest;
+  // 2: Asignar los QuadTree creados a cada hijo
+  this.northeast = qt_northeast;
+  this.northwest = qt_northwest;
+  this.southeast = qt_southeast;
+  this.southwest = qt_southwest;
 
-    // 3.- Hacer: this.divided <- true
+  // 3.- Hacer: this.divided <- true
+}
+
+insert(point) {
+  // Algoritmo
+  // 1: Si el punto no esta en los limites ( boundary ) del quadtree Return
+  // 2: Si ( this.points.length ) < ( this.capacity ),
+  // 2.1 Insertamos en el vector this.points
+  // Sino
+  // 2.2 Dividimos si aun no ha sido dividido
+  // 2.3 Insertamos recursivamente en los 4 hijos.
+  // this.northeast.insert ( point );
+  // this.northwest.insert ( point );
+  // this.southeast.insert ( point );
+  // this.southwest.insert ( point );
+}
+
+show() {
+  stroke(255);
+  strokeWeight(1);
+  noFill();
+  rectMode(CENTER);
+  rect(
+    this.boundary.x,
+    this.boundary.y,
+    this.boundary.w * 2,
+    this.boundary.h * 2
+  );
+  if (this.divided) {
+    this.northeast.show();
+    this.northwest.show();
+    this.southeast.show();
+    this.southwest.show();
   }
-
-  insert(point) {
-    // Algoritmo
-    // 1: Si el punto no esta en los limites ( boundary ) del quadtree Return
-    // 2: Si ( this.points.length ) < ( this.capacity ),
-    // 2.1 Insertamos en el vector this.points
-    // Sino
-    // 2.2 Dividimos si aun no ha sido dividido
-    // 2.3 Insertamos recursivamente en los 4 hijos.
-    // this.northeast.insert ( point );
-    // this.northwest.insert ( point );
-    // this.southeast.insert ( point );
-    // this.southwest.insert ( point );
+}
+// verifica si este objeto se intersecta con otro objeto Rectangle
+intersects(range) {
+  if (range.x == (this.x - this.w) || range.x == (this.x + this.w) || range.y == (this.y - this.h) || range.y == (this.y + this.h)) {
+    return true;
   }
+  return false;
 
-  show() {
-    stroke(255);
-    strokeWeight(1);
-    noFill();
-    rectMode(CENTER);
-    rect(
-      this.boundary.x,
-      this.boundary.y,
-      this.boundary.w * 2,
-      this.boundary.h * 2
-    );
-    if (this.divided) {
-      this.northeast.show();
-      this.northwest.show();
-      this.southeast.show();
-      this.southwest.show();
-    }
-  }
-    // verifica si este objeto se intersecta con otro objeto Rectangle
-    intersects(range) {
-        if (range.x == (this.x - this.w) || range.x == (this.x + this.w) || range.y == (this.y - this.h) || range.y == (this.y + this.h)) {
-            return true;
-        }
-        return false;
-
-    }
+}
 
