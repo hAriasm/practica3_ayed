3556a6ed977845535a3ef59f59f4debf969fb56f
hAriasm <hariasm@unsa.edu.pe>
2022-08-29 23:36:37 -0500

    implementacion insert

--- quadtree/sketch.js
+++ quadtree/sketch.js
@@ -1,21 +1,21 @@
 let qt;
- let count = 0;
+let count = 0;
 
- function setup () {
- createCanvas (400 ,400) ;
+function setup() {
+  createCanvas(400, 400);
 
- // centre point and half of width and height
- let boundary = new Rectangle (200 ,200 ,200 ,200) ;
+  // centre point and half of width and height
+  let boundary = new Rectangle(200, 200, 200, 200);
 
- // each leave just could have 4 elements
- qt = new QuadTree ( boundary , 4) ;
+  // each leave just could have 4 elements
+  qt = new QuadTree(boundary, 4);
 
- console.log (qt);
-for (let i=0; i < 3; i ++) {
-     let p = new Point ( Math.random () * 400 , Math.random () * 400) ;
-     qt.insert (p);
-     }
-    
-    background (0) ;
-     qt.show () ;
-     }
+  console.log(qt);
+  for (let i = 0; i < 3; i++) {
+    let p = new Point(Math.random() * 400, Math.random() * 400);
+    qt.insert(p);
+  }
+
+  background(0);
+  qt.show();
+}
