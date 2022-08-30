ab5846a6dc862eb907a8441c45630bed0dd78643
Edison Pérez <edisonaqp@gmail.com>
2022-08-30 12:04:17 -0500

    fix of boundary height

--- quadtree/sketch.js
+++ quadtree/sketch.js
@@ -11,7 +11,7 @@ function setup() {
   qt = new QuadTree(boundary, 4);
 
   console.log(qt);
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 4; i++) {
     let p = new Point(Math.random() * 400, Math.random() * 400);
     qt.insert(p);
   }
