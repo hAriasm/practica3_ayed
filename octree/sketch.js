c196fbd17a00cc639b85ecc8e70c8c8f756f8d2d
hAriasm <hariasm@unsa.edu.pe>
2022-09-02 00:13:04 -0500

    modificacion informe

--- octree/sketch.js
+++ octree/sketch.js
@@ -2,7 +2,7 @@ let ot;
 let count = 0;
 
 function setup() {
-  createCanvas(1200, 1200, WEBGL);
+  createCanvas(400, 400, WEBGL);
 
   // centre point and half of width and height
   let surface = new Cube(200, 200, 200, 200, 200, 200);
@@ -12,34 +12,27 @@ function setup() {
 
   console.log(ot);
   for (let i = 0; i < 10; i++) {
-    let p = new Point(
-      Math.random() * 400,
-      Math.random() * 400,
-      Math.random() * 400
-    );
+    let p = new Point(Math.random() * 400, Math.random() * 400, Math.random() * 400);
     ot.insert(p);
   }
 }
 
 function draw() {
   if (mouseIsPressed) {
-    let m = new Point(Math.random() * 400,
-    Math.random() * 400,
-    Math.random() * 400);
+    //let m = new Point(mouseX, mouseY);
     ot.insert(m);
-    mouseIsPressed = false;
   }
   background(0);
   ot.show();
   stroke(0, 255, 0);
   rectMode(CENTER);
-  let range = new Cube(mouseX, mouseY, mouseX, mouseX);
-  box(range.x, range.y, range.z, range.w * 2, range.h * 2, range.d * 2);
+  let range = new Cube(mouseX, mouseY, 25, 25);
+  box(range.x, range.y,range.z, range.w * 2, range.h * 2,range.d*2);
   let points = [];
   ot.query(range, points);
   for (let p of points) {
     strokeWeight(4);
-    point(p.x, p.y, p.z);
+    point(p.x, p.y, p.z)
   }
   //rect(300, 300, 107, 92);
 }
