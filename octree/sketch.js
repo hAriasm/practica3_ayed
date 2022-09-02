b03eee9859d8e5a40568fd41ad805dccb04fed9f
HARMOR286 <haranzamendi@unsa.edu.pe>
2022-09-02 00:05:01 -0500

    prueba de visualizacion octree

--- octree/sketch.js
+++ octree/sketch.js
@@ -2,7 +2,7 @@ let ot;
 let count = 0;
 
 function setup() {
-  createCanvas(1200, 1200, WEBGL);
+  createCanvas(800, 800, WEBGL);
 
   // centre point and half of width and height
   let surface = new Cube(200, 200, 200, 200, 200, 200);
@@ -22,24 +22,14 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    let m = new Point(Math.random() * 400,
-    Math.random() * 400,
-    Math.random() * 400);
-    ot.insert(m);
-    mouseIsPressed = false;
-  }
+
   background(0);
   ot.show();
-  stroke(0, 255, 0);
-  rectMode(CENTER);
-  let range = new Cube(mouseX, mouseY, mouseX, mouseX);
-  box(range.x, range.y, range.z, range.w * 2, range.h * 2, range.d * 2);
-  let points = [];
-  ot.query(range, points);
-  for (let p of points) {
-    strokeWeight(4);
-    point(p.x, p.y, p.z);
-  }
+  noFill();
+  stroke(255);
+  strokeWeight(0.5);
+  orbitControl()
+  box(400)
+  
   //rect(300, 300, 107, 92);
 }
