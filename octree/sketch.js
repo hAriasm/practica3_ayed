cadc7f779417484305e302e07b6d03f6c034a93e
Edison Pérez <edisonaqp@gmail.com>
2022-09-03 09:04:20 -0500

    versión final

--- octree/sketch.js
+++ octree/sketch.js
@@ -17,15 +17,19 @@ function setup() {
   // each leave just could have 4 elements
   ot = new OcTree(surface, 2);
 
-  // orientacion ejes x, y, z
+  // orientacion ejes x, y, zstroke(255, 0, 0);
   // ot.insert(new Point(50, 1, 1));
-  // ot.insert(new Point(1, 100, 1));
-  // ot.insert(new Point(1, 1, 25));
   // ot.insert(new Point(100, 1, 1));
+  // ot.insert(new Point(1, 100, 1));
   // ot.insert(new Point(1, 200, 1));
+  // ot.insert(new Point(1, 1, 25));
+  // ot.insert(new Point(1, 1, 50));
+  // ot.insert(new Point(1, 1, 75));
+  // ot.insert(new Point(1, 1, 100));
+  // ot.insert(new Point(1, 1, 150));
   // ot.insert(new Point(1, 1, 200));
   console.log(ot);
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 6; i++) {
     let p = new Point(Number(Math.random() * WIDTH).toFixed(0), Number(Math.random() * WIDTH).toFixed(0), Number(Math.random() * WIDTH).toFixed(0));
     ot.insert(p);
   }
@@ -46,7 +50,6 @@ function draw() {
   strokeWeight(1);
   orbitControl()
 
-  // if (mouseButton === RIGHT) {
   let range = new Cube(mouseX, mouseX, mouseX, 100, 100, 100);
 
   stroke(255, 0, 0);
